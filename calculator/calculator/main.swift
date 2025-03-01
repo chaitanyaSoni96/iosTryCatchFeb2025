a39f7fe0f8e11937f0deb903be218c5888114528
your_username <sonichaitanya13@gmail.com>
2025-03-01 12:38:52 +0530

    assignment: Calculator

---
+++ calculator/calculator/main.swift
@@ -0,0 +1,17 @@
+//
+//  main.swift
+//  calculator
+//
+//  Created by Chaitanya Soni on 01/03/25.
+//
+
+import Foundation
+
+print("Hello, World!")
+
+print("Whats your name?: ", terminator: "")
+if let name: String = readLine() {
+    
+    print("Hello \(name)")
+    
+}
